e67f8b8dedc04f0ffda9903d0d7e30b5d3208ec3
shaaz212 <shahabasha212@gmail.com>
2023-11-02 13:10:16 +0530

    bg-color-change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const green = (e)=>{
+    e.preventDefault()
+    const body = document.getElementById('bg')
+    let color = "green"
+    body.style.background = color
+  }
+
+  const yellow = (e)=>{
+    e.preventDefault()
+    const body = document.getElementById('bg')
+    let color = "yellow"
+    body.style.background = color
+  }
+
+  const red = (e)=>{
+    e.preventDefault()
+    const body = document.getElementById('bg')
+    let color = "red"
+    body.style.background = color
+  }
+
+  const blue = (e)=>{
+    e.preventDefault()
+    const body = document.getElementById('bg')
+    let color = "blue"
+    body.style.background = color
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-black w-screen h-screen flex justify-center items-center'>
+      <div id='bg' className='w-3/5 h-3/5 flex justify-center items-center'>
+        <Button variant="outline-success" className='me-3 font-extrabold' onClick={(e)=>green(e)}>Green</Button>
+        <Button variant="outline-warning" className='me-3 font-extrabold' onClick={(e)=>yellow(e)}>Yellow</Button>
+        <Button variant="outline-danger" className='me-3 font-extrabold' onClick={(e)=>red(e)}>Red</Button>
+        <Button variant="outline-info" className='font-extrabold' onClick={(e)=>blue(e)}>Blue</Button>
+      </div>
     </div>
   );
 }
